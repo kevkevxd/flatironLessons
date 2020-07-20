54240562bfb74a5ebcb121d80824864ff566dd9f
kevin_xie <xie.kevin.j@gmail.com>
2020-07-20 14:06:06 -0400

    mock challenge

--- console.rb
+++ console.rb
@@ -2,6 +2,19 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+
+#   Book.new(self, title, word_count)
+
+b1 = Book.new("turtle alert", 3232, "kevin")
+b2 = Book.new("coding for potatoes", 63232, "victor")
+b3 = Book.new("indigo plateau", 7432, "kevin")
+b4 = Book.new("how to cook hot-dog", 3117, "victor")
+
+a1 = Author.new("kevin")
+a2 = Author.new("victor")
+
+
 binding.pry
 
-
+puts "potato"
