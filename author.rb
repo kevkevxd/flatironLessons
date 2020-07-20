78edae6b7fb695542200823439bac9fb48bd1f0b
kevin_xie <xie.kevin.j@gmail.com>
2020-07-20 14:11:27 -0400

    code challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author
+
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    #selects all books of this author and creates an array
+    def books 
+        Book.all.select{|b| b.author == self }
+    end
+    ##struggled in pry here trying to call a1/a2 from console. Was returning an empty array
+
+
+    def write_book(title, word_count) #create a new book instance
+        Book.new(title, word_count, self)
+    end
+
+    #total number of words author wrote across authored books
+    def total_words 
+       books.sum(&:word_count)
+    end
+    ## felt pretty good about this, but couldn't test because of empty array in pry
+    ## also, may I have feedback on if using (&:) is acceptable? 
+        
+    def self.most_words #author instance who has written the most words
+        Book.all.max { |a, b| b.author.total_words <=> a.author.total_words }
+    end
+    ##iterating through each book instance to compare max total word count
+end
 
