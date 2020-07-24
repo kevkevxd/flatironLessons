cc4d542b66d3defdbbf8a45a0986beb6fc8254f8
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-24 15:02:27 -0400

    ryan werner adds

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,7 @@
 class Teacher < ActiveRecord::Base
     has_many :grade_levels
     has_many :students, through: :grade_levels
+    belongs_to :school
 
     def tenure
         if self.years_of_experience >5
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_041737) do
+ActiveRecord::Schema.define(version: 2020_07_24_185111) do
 
   create_table "grade_levels", force: :cascade do |t|
     t.integer "student_id"
     t.integer "teacher_id"
   end
 
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.string "mascot"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "grade_level"
+    t.integer "school_id"
   end
 
   create_table "teachers", force: :cascade do |t|
     t.string "last_name"
     t.string "grade_level"
     t.integer "years_of_experience"
+    t.integer "school_id"
   end
 
 end
