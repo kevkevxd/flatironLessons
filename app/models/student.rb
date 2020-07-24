87d53c8af10de86e5e39d791c981bdf9eb575d64
kevin_xie <xie.kevin.j@gmail.com>
2020-07-23 23:16:58 -0400

    it'll get better

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,29 @@
 class Student < ActiveRecord::Base
-end
+    has_many :grade_level
+    belongs_to :teachers, through: :grade_level
+
+    def full_name
+        "#{self.first_name}+ " " + #{self.last_name}"
+    end
+
+    def self.all_in_grade(grade_level)
+        Student.select {|s|s.grade_level == grade_level}
+    end
+end  
+    
+ 
+
+    # def grade 
+    #     Grade.all.select do |g|
+    #         g.student_id==self.id
+    #     end
+    # end
+
+    # def teachers
+    #     Teacher.all.select do |t|
+    #         self.grade.select do |g|
+    #             g.teacher_id == t.id
+    #         end
+    #     end
+    # end
+# end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,27 @@
 class Teacher < ActiveRecord::Base
+    has_many :grade_level
+    has_many :students, through: :grade_level
+
+    def tenure
+        if self.years_of_experience >5
+            true
+        else
+            false
+        end
+    end
+
+    # def grades
+    #     Grade.all.select do |g|
+    #         g.teacher_id==self.id
+    #     end
+    # end 
+    
+    # def students
+    #     Student.all.select do |t|
+    #         self.grade.select do |g|
+    #             g.student_id == t.id
+    #         end
+    #     end
+    # end
+
 end
