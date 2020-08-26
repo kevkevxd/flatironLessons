5e1a05b5c25c8a3b44b3025abda1a4afdb74dab7
DBenson1017 <devinbenson@gmail.com>
2020-08-26 10:23:59 -0400

    first eventListener not yet functional

--- index.js
+++ index.js
@@ -2,5 +2,52 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+
+let moveList = document.querySelector('ul');
+moveList.addEventListener('keydown', function(e){
+
+let direction = document.createElement('li')
+
+  if(e.key === ArrowLeft){
+    direction.innerContent = move('left');
+  }
+
+  if(e.key === ArrowRight){
+    direction.innerContent = move('right');
+    // direction.innerHTML = <p>Move Right</p>
+  }
+
+  if(e.key === ArrowDown){
+    direction.innerContent = move('down');
+  }
+
+  if(e.key === ArrowUp){
+    direction.innerContent = move('up');
+  }
+  
+
+//add to end of list
+moveList.appendChild('move')
+
+});
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
