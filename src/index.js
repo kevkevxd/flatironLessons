864de10551ebe9b991230dfe97c60005ecd5588c
kevin_xie <xie.kevin.j@gmail.com>
2020-08-27 14:49:21 -0400

    1st deliv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 let addToy = false;
 
+function fetchToys() {
+  return fetch("http://localhost:3000/toys")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (toys) {
+      const toyCollection = document.querySelector("#toy-collection");
+      toys.forEach((character) => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = ` 
+        <h2>${character.name}</h2>
+        <img src="${character.image}" class="toy-avatar" />
+        <p>${character.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+      </div>`;
+
+        toyCollection.append(card);
+      });
+    });
+}
+
+fetchToys();
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  // fetch toy objects from api
+  // take them out of the array object
+  // add them to a new div class called "card" under toy-collection div
+  //
 });
